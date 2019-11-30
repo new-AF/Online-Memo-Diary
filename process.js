f80b5d618e6f6f8099e419a7fa871c5b415b90c8
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2019-11-30 21:32:52 +0100

    Updated index.html style.css process.js

--- process.js
+++ process.js
@@ -1,1 +1,13 @@
-//
+
+f = () =>
+  {console.log(123);$(this).css("background-color", "#cccccc");}
+
+f3 = ()=> $('button').show()
+f2 = ()=> $('button').toggle('slow',f3)
+
+$(
+  function(){
+  console.log("test")
+  $('button').mouseover(f2)
+}
+)
