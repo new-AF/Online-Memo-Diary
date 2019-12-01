b84f85ce2790afb95ced9cbff5905a2980e0bcc0
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2019-12-01 02:04:46 +0100

    Updated index.html process.js

--- process.js
+++ process.js
@@ -1,13 +1,85 @@
 
-f = () =>
-  {console.log(123);$(this).css("background-color", "#cccccc");}
+d = document
 
-f3 = ()=> $('button').show()
-f2 = ()=> $('button').toggle('slow',f3)
+/*A compliment to jQ's .prop*/
 
+function PropCpy(x,y) {
+  console.log('--->>',x,y)
+  console.log('--->>',x,y)
+
+  x.prop('xxAttr').forEach( i => {
+    console.log( '*******',i )
+    y.prop(i, x.prop(i) )
+  } )
+  console.log( '*******',x.prop('xxAttr') )
+}
+
+function put(x) {
+  y = x instanceof Array ? x : [x]
+  y.forEach( (i) => console.log (i))
+}
+function undef(x,y) {
+  //console.log(`[${y}]`)
+  return x !== undefined ? x : (y === undefined ? "[**LAST RESORT X**]" : y)
+}
+
+function NoProp(x,name,value) {
+
+  value = undef(value , 'NoProp')
+
+  if (!x.prop) {
+    try {
+      x = $(x)
+    }
+    catch (error) {
+      console.log('[function put]',error);
+      return;
+    }
+  }
+
+    if (!x.prop(name)) {
+      if (value.prop) {
+        x.prop(name,value.prop(name))
+      }
+      else {
+        x.prop(name,value)
+      }
+
+    }
+
+}
+function SetCount(x,y) {
+
+  //console.log(`[SETCOUNT] x=[${x}] y=[${y}]`)
+  name = 'xxCount'
+
+  NoProp(x,name,undef(y,0))
+
+  //console.log(prop(x))
+  /*console.log(x)
+  console.log( x.prop('Count') )
+  console.log( x.text() )*/
+  //console.log( x, x.text(),x.prop('xxCount') )
+  x.prop(name, x.prop(name)+1 )
+  x.text(x.text().split(' ')[0] + " " + x.prop(name) )
+}
+
+function Add(e) {
+  x = $(e.target)
+  p = x.parent()
+  last = p.children().not(x).last()
+
+  n = last.clone(true)
+
+
+  SetCount(n,last)
+
+  n.insertBefore(x)
+
+}
 $(
-  function(){
-  console.log("test")
-  $('button').mouseover(f2)
+  () => {
+  SetCount ( $('.Page-Button') )
+  $('.L , .Add').click(Add)
 }
 )
